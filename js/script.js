d0735952359d77a23d74fe9c7f032e69465511a1
Joanne Arboleda <Jo@Sams-MacBook-Pro.local>
2019-04-04 15:04:40 -0700

    hover effects

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
-$( document ).ready({
-    
-})
+$( document ).ready(function() {
+    $( "#menu-icon" ).click(function() {
+        $( ".mobile-menu" ).fadeIn( 200 );
+
+        $( "body" ).css( "overflow", "hidden" );
+    });
+
+    $( ".mobile-menu .icon" ).click(function() {
+        $( ".mobile-menu" ).fadeOut( 200 );
+
+        $( "body" ).css( "overflow", "visible" );
+    });
+
+    var currentScroll = $(window).scrollTop();
+
+    $(window).scroll(function(){
+        //$(".clouds .left").css("opacity", 1 - $(window).scrollTop() / 300);
+        var newScroll = $(window).scrollTop();
+        console.log( currentScroll );
+
+        console.log( newScroll );
+
+        if( newScroll < currentScroll ) {
+            $("nav").fadeIn( 200 );
+        } else {
+            $("nav").fadeOut( 200 );
+        }
+
+        currentScroll = newScroll;
+    });    
+});
